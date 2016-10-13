2d9d9d8269f20aa2108204f81f9433849b3b2f29
Emanuel Dima <emanueldima@gmail.com>
2016-10-13 14:50:01 +0200

    Initial Commit

---
+++ IJKLarrows/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IJKLarrows
+//
+//  Created by Emanuel Dima on 13/10/16.
+//  Copyright © 2016 Emanuel Dima. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
